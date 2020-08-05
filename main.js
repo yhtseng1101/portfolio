37dfeebb35dc900818dc6beb1aa5aa1b583bad7c
yhtseng1101 <yhtseng1101@gmail.com>
2020-08-05 00:47:14 -0500

    turn on modal

--- main.js
+++ main.js
@@ -27,21 +27,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // toggel modal
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const modal = document.getElementById("modal");
-//   setTimeout(function() {
-//       modal.classList.toggle("is-active");
-//   }, 1000);
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById("modal");
+  setTimeout(function() {
+      modal.classList.toggle("is-active");
+  }, 1000);
 
-// })
+})
 
-// const closeBtn = document.getElementById("closeBtn");
-// closeBtn.onclick = function() {
-//   modal.classList.remove("is-active");
-// }
+const closeBtn = document.getElementById("closeBtn");
+closeBtn.onclick = function() {
+  modal.classList.remove("is-active");
+}
 
-// const closeBG = document.getElementsByClassName("modal-background")[0];
-// closeBG.onclick = function() {
-//   modal.classList.remove("is-active");
-// }
+const closeBG = document.getElementsByClassName("modal-background")[0];
+closeBG.onclick = function() {
+  modal.classList.remove("is-active");
+}
 
